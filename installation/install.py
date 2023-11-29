668352954dc43558e7d103f6f0f0c6a5409911ec
rithikasilva <53627672+rithikasilva@users.noreply.github.com>
2023-11-29 11:42:49 -0500

    Add outline for installation script

---
+++ installation/install.py
@@ -0,0 +1,23 @@
+'''
+STARSHIP:
+- Install Starship:
+    - Run installation cmd
+      `curl -sS https://starship.rs/install.sh | sh`
+    - Add stow my config
+    - Add line in .bashrc if not already there
+'''
+
+
+'''
+TMUX:
+- Ensure TMUX is installed with the correct version
+- stow my config (and associated plugins)
+'''
+
+'''
+NVIM:
+- Install rustup
+- Remove NVIM if already installed and version < 0.8.0
+- Install NVIM using bob with version >= 0.8.0
+- Stow my config
+'''
