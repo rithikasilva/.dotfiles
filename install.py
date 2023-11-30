fbc1eb5a59ed6e70d000687e3c27b38ab3838b0d
rithikasilva <53627672+rithikasilva@users.noreply.github.com>
2023-11-30 10:19:08 -0500

    Isolating install components

--- install.py
+++ install.py
@@ -1,6 +1,13 @@
 import os
 from pathlib import Path
 import subprocess
+
+
+def setup():
+    os.system("sudo apt update")
+    os.system("sudo apt install stow")
+
+
 '''
 STARSHIP:
 - Install Starship:
@@ -9,21 +16,23 @@ STARSHIP:
     - Add stow my config
     - Add line in .bashrc if not already there
 '''
-if os.system("curl -sS https://starship.rs/install.sh | sh") != 0:
-    print("Couldn't install Starship")
-    exit()
-if os.system("stow starship") != 0:
-    print("Couldn't apply Starship config")
-    exit()
 
-try:
-    with open(Path.home() / ".bashrc", "a+") as f:
-        bash_eval = 'eval "$(starship init bash)"'
-        if bash_eval not in f.read():
-            f.write(bash_eval)
-except Exception as e:
-    print("Error with .bashrc Starship check/appending:")
-    print(e)
+
+def install_starship():
+    if os.system("curl -sS https://starship.rs/install.sh | sh") != 0:
+        print("Couldn't install Starship")
+        exit()
+    if os.system("stow starship") != 0:
+        print("Couldn't apply Starship config")
+        exit()
+    try:
+        with open(Path.home() / ".bashrc", "a+") as f:
+            bash_eval = 'eval "$(starship init bash)"'
+            if bash_eval not in f.read():
+                f.write(bash_eval)
+    except Exception as e:
+        print("Error with .bashrc Starship check/appending:")
+        print(e)
 
 
 '''
@@ -31,20 +40,24 @@ TMUX:
 - Ensure TMUX is installed with the correct version
 - stow my config (and associated plugins)
 '''
-try:
-    tmux_version = float(subprocess.check_output(
-        ['tmux', '-V'], text=True).split(' ')[1].replace("a", ""))
-    if tmux_version >= 3.0:
-        if os.system("stow tmux") != 0:
-            print("Couldn't apply tmux config")
-    else:
-        print(f"tmux version is below 3.0: {tmux_version}")
-except (subprocess.CalledProcessError, FileNotFoundError):
-    if os.system("sudo apt install tmux") != 0:
-        print("Coudlnt' install tmux")
-    else:
-        if os.system("stow tmux") != 0:
-            print("Couldn't apply tmux config")
+
+
+def install_tmux():
+    try:
+        tmux_version = float(subprocess.check_output(
+            ['tmux', '-V'], text=True).split(' ')[1].replace("a", ""))
+        if tmux_version >= 3.0:
+            if os.system("stow tmux") != 0:
+                print("Couldn't apply tmux config")
+        else:
+            print(f"tmux version is below 3.0: {tmux_version}")
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        if os.system("sudo apt install tmux") != 0:
+            print("Coudlnt' install tmux")
+        else:
+            if os.system("stow tmux") != 0:
+                print("Couldn't apply tmux config")
+
 
 '''
 NVIM:
@@ -53,3 +66,8 @@ NVIM:
 - Install NVIM using bob with version >= 0.8.0
 - Stow my config
 '''
+
+
+setup()
+install_starship()
+install_tmux()
