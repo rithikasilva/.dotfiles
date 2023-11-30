eb7174c5a127f8f20c60a181b9f4d6d392486d14
rithikasilva <53627672+rithikasilva@users.noreply.github.com>
2023-11-30 10:35:21 -0500

    Initial nvim stuff for install

--- install.py
+++ install.py
@@ -22,10 +22,10 @@ STARSHIP:
 def install_starship():
     if os.system("curl -sS https://starship.rs/install.sh | sh") != 0:
         print("Couldn't install Starship")
-        exit()
+        return False
     if os.system("stow starship") != 0:
         print("Couldn't apply Starship config")
-        exit()
+        return False
     try:
         with open(Path.home() / ".bashrc", "a+") as f:
             bash_eval = 'eval "$(starship init bash)"'
@@ -34,6 +34,8 @@ def install_starship():
     except Exception as e:
         print("Error with .bashrc Starship check/appending:")
         print(e)
+        return False
+    return True
 
 
 '''
@@ -62,6 +64,8 @@ def install_tmux():
 
 '''
 NVIM:
+- Ensure NVIM isn't already installed
+
 - Install rustup
 - Remove NVIM if already installed and version < 0.8.0
 - Install NVIM using bob with version >= 0.8.0
@@ -69,6 +73,25 @@ NVIM:
 '''
 
 
+def install_nvim():
+    os.system("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh")
+    os.system('source "$HOME/.cargo/env"')
+    os.system("cargo install bob-nvim")
+    os.system("bob use 0.9.0")
+    bob_path = 'export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"'
+    try:
+        with open(Path.home() / ".bashrc", "a+") as f:
+            if bob_path not in f.read():
+                f.write(bob_path)
+    except Exception as e:
+        print("Error with installing bob-nvim:")
+        print(e)
+        return False
+    os.system("stow nvim")
+    return True
+
+
 setup()
 install_starship()
 install_tmux()
+install_nvim()
