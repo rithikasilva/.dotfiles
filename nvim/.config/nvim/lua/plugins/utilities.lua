bed2e295e9613f143a7d6888b740426a0d826868
rithikasilva <rithikasilva@outlook.com>
2025-02-13 15:48:44 -0500

    Adding tmux a sessionizer

--- nvim/.config/nvim/lua/plugins/utilities.lua
+++ nvim/.config/nvim/lua/plugins/utilities.lua
@@ -117,30 +117,6 @@ return {
 		},
 	},
 	{
-		"nvim-neorg/neorg",
-		lazy = false,
-		version = "*",
-		config = function()
-			require("neorg").setup {
-				load = {
-					["core.defaults"] = {},
-					["core.concealer"] = {},
-					["core.dirman"] = {
-						config = {
-							workspaces = {
-								notes = "~/notes",
-							},
-							default_workspace = "notes",
-						},
-					},
-				},
-			}
-
-			vim.wo.foldlevel = 99
-			vim.wo.conceallevel = 2
-		end,
-	},
-	{
 		"3rd/image.nvim",
 		dependencies = { "luarocks.nvim" },
 	},
