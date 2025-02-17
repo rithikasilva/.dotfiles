a8577238b815ad6753646ef63a126ceb10f31754
rithikasilva <rithikasilva@outlook.com>
2025-02-16 21:58:35 -0500

    Small QOL

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -106,6 +106,11 @@ vim.api.nvim_set_keymap('n', '<A-\\>', ':vsplit<CR>', { noremap = true, silent =
 vim.api.nvim_set_keymap('n', '<A-->', ':split<CR>', { noremap = true, silent = true })
 
 
+-- Auto center on Ctrl+u and Ctrl+d
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {noremap = true, silent = true})
+
+
 -- Move current line up with Alt+j
 vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
