170a238a2f847a39e8d914fd1d613dda6b858db2
rithikasilva <rithikasilva@outlook.com>
2025-04-29 18:49:45 -0400

    Updated gitsigns bindings

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -39,23 +39,22 @@ return {
 					return '<Ignore>'
 				end, { expr = true, desc = 'Jump to previous hunk' })
 
-				map('v', '<leader>hs', function()
-					gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-				end, { desc = 'stage git hunk' })
-				map('v', '<leader>hr', function()
-					gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-				end, { desc = 'reset git hunk' })
 				-- normal mode
-				map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+				map('n', '<leader>hn', gs.next_hunk, {desc = 'git next hunk'})
+				map('n', '<leader>hp', gs.prev_hunk, {desc = 'git previous hunk'})
+
 				map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
+				map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
 				map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
 				map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
 				map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-				map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+
+				map('n', '<leader>hP', gs.preview_hunk_inline, { desc = 'preview git hunk' })
 				map('n', '<leader>hb', function()
 					gs.blame_line { full = false }
 				end, { desc = 'git blame line' })
 				map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+				map('n', '<leader>hx', ':diffoff! | wincmd p | q | wincmd p<CR>', { desc = "Close git diff view" })
 				map('n', '<leader>hD', function()
 					gs.diffthis '~'
 				end, { desc = 'git diff against last commit' })
