428eaac5c5ecbd3677a5ff7f7567cd3faafcf27e
rithikasilva <rithikasilva@outlook.com>
2025-05-18 16:54:33 -0700

    nvim: Redo lsp for 0.11

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,16 +2,19 @@ return {
 	{
 		'neovim/nvim-lspconfig',
 		dependencies = {
-			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason.nvim',
 			'williamboman/mason-lspconfig.nvim',
+			{ 'j-hui/fidget.nvim', opts = {} },
 			{
-				-- For displaying lsp messages in bottom-right pop-up
-				'j-hui/fidget.nvim',
+				"folke/lazydev.nvim",
+				ft = "lua",
 				opts = {
-					notification = { window = { winblend = 0 } },
-				}
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
 			},
-			'folke/neodev.nvim',
+			'stevearc/conform.nvim',
 		},
 		config = function()
 			local on_attach = function(_, bufnr)
@@ -26,113 +29,81 @@ return {
 				nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 			end
 
-			require('mason').setup()
-			require('mason-lspconfig').setup()
-			require('neodev').setup()
-
-			local servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							workspace = { checkThirdParty = false },
-							telemetry = { enable = false },
-						},
-					},
-				},
-				ltex = {
-					settings = {
-						ltex = {
-							language = "en-CA",
-							dictionary = {
-								["en-CA"] = { "Neovim", "neovim", "Rithika", "Silva" },
-							},
-						},
-					},
-				},
-			}
-
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-			local mason_lspconfig = require 'mason-lspconfig'
+			local lspconfig = require("lspconfig")
+			lspconfig.ltex.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					ltex = {
+						language = "en-CA",
+						dictionary = {
+							["en-CA"] = { "Neovim", "neovim", "Rithika", "Silva" },
+						},
+					},
+				}
+			})
 
-			mason_lspconfig.setup {
-				ensure_installed = vim.tbl_keys(servers),
-				automatic_installation = false,
-			}
+			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
+					},
+				},
+			})
 
-			mason_lspconfig.setup_handlers {
-				function(server_name)
-					local opts = {
-						capabilities = capabilities,
-						on_attach = on_attach,
-					}
+			vim.lsp.config('jedi_language_server', {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
 
-					-- Merge server-specific settings if available
-					if servers[server_name] then
-						for k, v in pairs(servers[server_name]) do
-							opts[k] = v
-						end
-					end
+			vim.lsp.config("clangd", {
+				on_attach = on_attach,
+				capabilities = capabilities
+			})
 
-					require('lspconfig')[server_name].setup(opts)
-				end,
-			}
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "black" },
+					rust = { "rustfmt" },
+					cpp = { "clang_format_custom" },
+					c = { "clang_format_custom" },
+					json = { "clang_format_custom" },
+				},
+				formatters = {
+					black = {
+						args = { "--line-length", "100", "-" },
+					},
+					clang_format_custom = {
+						command = "clang-format",
+						args = function(_, ctx)
+							local cwd = vim.fn.getcwd()
+							local config_exists = vim.fn.globpath(cwd, ".clang-format") ~= ""
+
+							if config_exists then
+								return { "-style=file" }
+							else
+								return {
+									"-style={BasedOnStyle: LLVM, IndentWidth: 2, TabWidth: 2, UseTab: ForIndentation}"
+								}
+							end
+						end,
+						stdin = true,
+					},
+				},
+			})
+
+			vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("conform").format()<CR>',
+				{ noremap = true, silent = true })
 		end,
 	},
-	{
-		-- I want to use formatters that aren't tied to my lsp
-		'nvimdev/guard.nvim',
-		dependencies = {
-			'nvimdev/guard-collection'
-		},
-		config = function()
-			local ft = require('guard.filetype')
-
-			ft('python'):fmt({
-				cmd = 'black',
-				args = { '--line-length', '100', '-' },
-				stdin = true,
-			})
-
-			-- I don't mind default, work has specifications
-			local function clang_format_cmd()
-				local cwd = vim.fn.getcwd()
-				local config_exits = vim.fn.globpath(cwd, ".clang-format") ~= ""
-
-				if config_exits then
-					return {
-						cmd = "clang-format",
-						args = { "-style=file" },
-						stin = true,
-					}
-				else
-					return {
-						cmd = "clang-format",
-						args = {
-							"-style={BasedOnStyle: LLVM, IndentWidth: 2, TabWidth: 2, UseTab: ForIndentation}"
-						},
-						stdin = true,
-					}
-				end
-			end
-
-			-- Need to edit this when dealing with a specific format file
-			ft('c,cpp,json'):fmt(clang_format_cmd)
-
-			ft('rust'):fmt({
-				cmd = 'rustfmt',
-				stdin = true,
-			})
-
-			vim.g.guard_config = {
-				-- Different places may use a separate format script, so prefer manual
-				fmt_on_save = false,
-				lsp_as_default_formatter = true,
-			}
-
-			-- Associated keybinding
-			vim.api.nvim_set_keymap('n', '<leader>ff', ':Guard fmt<CR>', { noremap = true, silent = true })
-		end,
-	}
 }
