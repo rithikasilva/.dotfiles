cb48c6bbcdc0cdd196b505ae09b11bfd5ed5a707
rithikasilva <rithikasilva@outlook.com>
2025-06-16 22:45:03 -0700

    neovim: add gp.nvim for LLM chat window

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -72,7 +72,7 @@ return {
 
 			require("conform").setup({
 				formatters_by_ft = {
-					lua = { "stylua" },
+					-- lua = { "stylua" },
 					python = { "black" },
 					rust = { "rustfmt" },
 					cpp = { "clang_format_custom" },
