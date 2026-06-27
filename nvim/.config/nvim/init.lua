6857d8185a102a7a75ab06be8853a43ef765a253
Rithika Silva <rithikasilva@outlook.com>
2026-06-26 20:22:10 -0400

    feat: switching nvim to 0.12 features

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,33 +1,45 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{
+		src = "https://github.com/christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<C-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<C-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<C-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<C-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+})
 
 
-require('lazy').setup({
-  require 'plugins.colourscheme',
-  require 'plugins.snacks',
-  require 'plugins.lsp',
-  require 'plugins.git',
-  require 'plugins.cmp',
-  require 'plugins.utilities',
-  require 'plugins.treesitter',
-}, {})
 
-vim.cmd.colorscheme("vague")
+-- require('lazy').setup({
+--   require 'plugins.colourscheme',
+--   require 'plugins.snacks',
+--   require 'plugins.lsp',
+--   require 'plugins.git',
+--   require 'plugins.cmp',
+--   require 'plugins.utilities',
+--   require 'plugins.treesitter',
+-- }, {})
+
 
 -- Some colourscheme overrides happen here
 require("vim-options")
-require("rg-to-qf")
+-- require("rg-to-qf")
+
+
+
+vim.cmd.colorscheme("vague")
 
